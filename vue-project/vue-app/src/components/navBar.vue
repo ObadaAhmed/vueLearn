87c6cc382b429660fc02af414720f1ed8a8e55a2
obada ahmed <aboodey33@gamil.com>
2020-02-19 08:53:47 +0200

    sdfsd

---
+++ vue-project/vue-app/src/components/navBar.vue
@@ -0,0 +1,52 @@
+<template>
+    <nav>
+         <v-toolbar  app class="text-uppercase grey--text">
+             <v-app-bar-nav-icon flat color="grey" @click="drawer = !drawer"></v-app-bar-nav-icon>
+             <v-toolbar-title>
+            <span class="font-weight-light">todo</span>
+            <span>obada</span>
+           </v-toolbar-title>
+            <v-spacer></v-spacer>
+        <v-btn flat color="grey">
+            <span>sign out</span>
+            <v-icon right="">exit_to_app</v-icon>
+        </v-btn>
+        </v-toolbar>
+
+        <v-navigation-drawer app class="primary" v-model="drawer">
+            <v-list >
+                <v-list-item class="white--text" v-bind:key="link.text" v-for="link in links" router :to="link.route" >
+                   <v-list-tile-action>
+                       <v-icon left class="white--text">{{link.icon}}</v-icon>
+                   </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title class="white--text">
+                            {{link.text}}
+                        </v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-item>
+                 </v-list>
+
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "navBar",
+        data(){
+            return {
+                drawer : false,
+                links :[
+                    { icon: "dashboard", text: "Dashboard", route: "/" },
+                    { icon: "folder", text: "My Projects", route: "/projects" },
+                    { icon: "person", text: "Team", route: "/team" }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-project/vue-app/src/views/team.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+    <h1 class="subheading grey--text">Team</h1>
+    <v-container fluid class="my-4">
+        this should be the content
+    </v-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "team"
+    }
+</script>
+
+<style scoped>
+
+</style>
